2bc2d98a1a415546e65f966e9b304962e7c23772
odairo3 <odairu.odairo@gmail.com>
2026-06-20 12:54:04 -0500

    añadido archivo test de recetas

---
+++ test/recipeLogic.test.ts
@@ -0,0 +1,36 @@
+import { describe, test, expect } from 'vitest';
+import { createRecipe, addComponent, removeComponent } from '../lib/recipeLogic';
+
+describe('Recipe Logic functions - RF_03', () => {
+    
+    describe('1. createRecipe', () => {
+    
+        // Aquí adentro irán nuestros bloques de pruebas
+        test('should do something specific', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+    });
+
+    describe('2. addComponent', () => {
+    
+        // Aquí adentro irán nuestros bloques de pruebas
+        test('should do something specific', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+    });
+
+    describe('3. removeComponent', () => {
+    
+    // Aquí adentro irán nuestros bloques de pruebas
+        test('should do something specific', () => {
+            // Arrange
+            // Act
+            // Assert
+        });
+    });
+
+});
